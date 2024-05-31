4f69926c36d7f6844c4383c22d4bb39c5a7a14b4
alexey.rokalo <alexey.rokalo@intradiem.com>
2024-05-31 10:49:05 +0200

    Changed current directory and parent
    Change-Id: I20c776b9d34c9da35e262c19fcf237a26f22d2c6

--- app/src/main/java/com/ar/wandertrack/MainActivity.kt
+++ app/src/main/java/com/ar/wandertrack/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.ar.wandertrack
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
--- app/src/test/java/com/ar/wandertrack/ExampleUnitTest.kt
+++ app/src/test/java/com/ar/wandertrack/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.ar.wandertrack
 
 import org.junit.Test
 
